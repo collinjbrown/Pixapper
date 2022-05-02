7030e45075b562ac125c5ef7d556d25a0cc09832
Collin Brown <CJBrown7966@gmail.com>
2022-05-01 18:19:03 -0700

    Initial Commit x2

---
+++ Pixapper.cpp
@@ -0,0 +1,18 @@
+﻿// Pixapper.cpp : Defines the entry point for the application.
+//
+
+#include "Pixapper.h"
+
+using namespace std;
+
+int main()
+{
+	// Thank you to Lode Vandevenne for providing Lodepng, without which I'd surely flounder.
+	// I'm providing Pixapper under the MIT license in part due to its quite niche use.
+	// It might be useful for others, but I'm designing it particularly for my own game,
+	// The Moonlight Tongue, which is a metroidvania that uses texture mapping to apply
+	// visual effects to animations and spice up certain sprites.
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
